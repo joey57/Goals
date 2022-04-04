236e582af7752d3fe0f8e5b047e472b22d68767e
joey57 <njorogejoey5@gmail.com>
2022-04-04 14:38:51 +0300

    added array and constructors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goal = 'Watch Finding Nemo';
+  goals:string[];
+
+  constructor(){
+    this.goals = ['Watching finding Nemo', 'Buy cookies', 'Get new phone case']
+  }
 }
