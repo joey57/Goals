516996316a705953da2bb7693cc18d1179ca4818
joey57 <njorogejoey5@gmail.com>
2022-04-05 14:18:45 +0300

    added goal completegoal function

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -18,6 +18,13 @@ export class GoalComponent implements OnInit {
   toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
+
+  completeGoal(isComplete, index){
+    if (isComplete){
+      this.goals.splice(index,1);
+    }
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
