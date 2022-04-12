8b1bee73a52e3b3489c5bb481d72b81561349fa3
joey57 <njorogejoey5@gmail.com>
2022-04-12 14:01:12 +0300

    changes to access only view a specific goal

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -5,6 +5,7 @@ import { GoalService } from '../goal-service/goal.service';
 import { AlertService } from '../alert-service/alert.service'; 
 import { QuoteRequestService } from '../quote-http/quote-request.service';
 import { Quote } from '../quote-class/quote';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-goal',
@@ -18,21 +19,22 @@ export class GoalComponent implements OnInit {
   goals:Goal[];
   alertService:AlertService;
   quote:Quote;
-  
-  toggleDetails(index){
-    this.goals[index].showDescription = !this.goals[index].showDescription;
-  }
 
-  deleteGoal(isComplete, index){
-    if (isComplete){
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)//creates a prompt 
+  goToUrl(id){
+    this.router.navigate(['/goals',id])
+  }
+  // toggleDetails(index){
+  //   this.goals[index].showDescription = !this.goals[index].showDescription;
+  // }
+  deleteGoal(index){
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)//creates a prompt 
 
       if(toDelete){
       this.goals.splice(index,1);// deletes a goal from child component
       this.alertService.alertMe("The goal has been deleted")
     }
-  }
 }
+
  addNewGoal(goal){
    let goalLength = this.goals.length;
    goal.id = goalLength+1;
@@ -40,7 +42,7 @@ export class GoalComponent implements OnInit {
    this.goals.push(goal)
  }
  
-  constructor(goalService:GoalService, alertService:AlertService, private http:HttpClient, private quoteService:QuoteRequestService) {
+  constructor(goalService:GoalService, alertService:AlertService, private http:HttpClient, private quoteService:QuoteRequestService,private router:Router) {
     this.goals = goalService.getGoals()
     this.alertService = alertService;
    }
